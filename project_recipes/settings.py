9a1ac2047547815e5c7c7b43e856bb5508489f87
João Filipe <joaofilipeb045@gmail.com>
2022-04-01 15:19:46 -0300

    Add templates and partials

--- project_recipes/settings.py
+++ project_recipes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b@gr7gp36ayza#pjkc_brror3ptn8#(g+m*@2x#lj&c4s-+c_n'
+SECRET_KEY = os.environ.get("SECRET_KEY", "/env/SEC RET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
